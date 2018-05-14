1c23b8bbd2a1946ed31b5afe832b88cc964eed9b
Matt Maher <mattmaher0@gmail.com>
2018-05-14 20:16:27 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,9 @@
-def prime(num)
-  (2..n/2).none?{|i| n % i == 0}
+def prime?(n)
+  return false if n < 2 
+  return true if n == 3 || n == 2 
+    if (2...n-1).any?{|i| n % i == 0}
+      false
+    else
+      true
+    end
 end
